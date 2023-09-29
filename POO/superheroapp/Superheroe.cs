a072e3a646393e58bd3a910b2d7a55964cce3b1f
Antonio <poblanoeslavaa@gmail.com>
2023-09-29 12:24:40 -0600

    programas node

--- POO/superheroapp/Superheroe.cs
+++ POO/superheroapp/Superheroe.cs
@@ -1,72 +1,33 @@
 ﻿
+
 using System.Text;
 
-var poderVolar = new Superpoder();
-poderVolar.Nombre = "volar";
-poderVolar.Descripcion = "volar en el aire";
-poderVolar.Nivel = NivelPoder.NivelDos;
-
-var spuerfuerza = new Superpoder();
-spuerfuerza.Nombre = "Super Fuerza";
-spuerfuerza.Nivel = NivelPoder.nivelTres;
-
-var reactorark = new Superpoder();
-reactorark.Nombre = "Reactor Ark";
-reactorark.Nivel = NivelPoder.nivelTres;
-
-var unirayo = new Superpoder();
-unirayo.Nombre = "Unirayo";
-unirayo.Nivel = NivelPoder.NivelDos;
-
-
-var superman = new Superheroe();
-var ironman = new Superheroe();
-
-superman.Id = 1;;
-superman.Nombre = "Superman";
-superman.IdentidadSecreta = "Clark Kent";
-superman.Ciudad = "Metropilos";
-superman.Puedevolar = true;
-
-var superman2 = new Superheroe();
-
-superman2.Id = 1;;
-superman2.Nombre = "Superman";
-superman2.IdentidadSecreta = "Clark Kent";
-superman2.Ciudad = "Metropilos";
-superman2.Puedevolar = true;
-
-Console.WriteLine(superman == superman2);
-
-SuperheroeRecord superheroeRecord = new (1, "Superman", "Clark Kent");
-SuperheroeRecord superheroeRecord2 = new (1, "Superman", "Clark Kent");
-
-Console.WriteLine(superheroeRecord == superheroeRecord2);
-
-//List<Superpoder> poderesSuperman = new List<Superpoder>();
-//poderesSuperman.Add(poderVolar);
-//poderesSuperman.Add(spuerfuerza);
-//superman.Superpoderes = poderesSuperman;
-//string resultSuperpoderes = superman.UsarSuperPoderes();
-//Console.WriteLine(resultSuperpoderes);
-
-ironman.Id = 2;
-ironman.Nombre = "Iron Man";
-ironman.IdentidadSecreta ="Tony Stark";
-ironman.Ciudad ="kansas";
-ironman.Puedevolar = true;
-
-//List<Superpoder> poderesironman = new List<Superpoder>();
-//poderesironman.Add(reactorark);
-//poderesironman.Add(unirayo);
-//ironman.Superpoderes = poderesironman;
-//string resul = ironman.UsarSuperPoderes();
-//Console.WriteLine(resul);
-class Superheroe
+class Superheroe : Heroe, Isuperheroe
 {
-    public int Id = 1;
-    public string Nombre;
-    public string IdentidadSecreta;
+    private string _Nombre;
+    public int Id { get; set; } = 1;
+    public override string Nombre
+    {
+        get
+     {
+        return _Nombre;
+     } 
+        set
+     {
+        _Nombre = value.Trim();
+     }
+    }
+
+    public string NombreEIDentidadSecreta
+    {
+        get 
+        {
+            return $"{Nombre} ({IdentidadSecreta})";
+        }
+    }
+
+
+    public string IdentidadSecreta { get; set; }
     public string Ciudad;
     public List<Superpoder> Superpoderes = new List<Superpoder>();
     public bool Puedevolar;
@@ -83,10 +44,20 @@ class Superheroe
         StringBuilder sb = new StringBuilder();
         foreach (var item in Superpoderes)
         {
-            sb.AppendLine($"{Nombre} esta usando el superpoder {item.Nombre}!!");
+            sb.AppendLine($"{NombreEIDentidadSecreta} esta usando el superpoder {item.Nombre}!!");
         }
         return sb.ToString();
     }
+    public override string SalvarElMundo()
+    {
+        return $"{NombreEIDentidadSecreta} ha salvado el mundo";
+    }
+    public override string SalvarLaTierra()
+    {
+        //return base.SalvarLaTierra();
+        return $"{NombreEIDentidadSecreta} ha salvado la tierra";
+    }
+
 }
 
 class Superpoder
@@ -109,3 +80,5 @@ enum NivelPoder
 }
 
 public record SuperheroeRecord(int Id, String Nombre, String IdentidadSecreta);
+
+
---
+++ node/entorno.js
@@ -0,0 +1,8 @@
+let nombre = process.env.NOMBRE || "Sin nombre";
+let nombre1 = process.env.NOMBRE1 || "Sin nombre";
+let web = process.env.WEB || "No tengo web";
+
+console.log("hola "+ nombre);
+console.log("hola " + nombre1);
+console.log("mi web es " + web);
+
