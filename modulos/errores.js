103d0a1a82affcd341d1051212921f9f4175b7da
Antonio <poblanoeslavaa@gmail.com>
2023-10-03 13:33:43 -0600

    cambios

--- modulos/errores.js
+++ modulos/errores.js
@@ -6,8 +6,23 @@ function serompe(){
     return 3 + z;
 }
 
+function serompeasincrona(cb){
+    setTimeout(function() {
+        try{
+            return 3 + z;
+
+        }catch(err){
+            console.error("Error en mi funcion asincrona");
+            cb(err.message);
+        }
+    });
+}
+
 try {
-    otrafuncion();
+    //otrafuncion();
+    serompeasincrona(function(){
+       console.log("Hay error");
+    });
 } catch(err){
     console.error("Vaya vaya simonn hay un error");
     console.error(err.message);
